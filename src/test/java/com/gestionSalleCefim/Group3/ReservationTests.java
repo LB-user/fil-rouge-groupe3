4b6c2136542c60188b18534602c5aa6fe4d5fea5
Njanati <nadablg5@gmail.com>
2023-05-10 13:19:17 +0200

    Reservation Jira 53

--- src/test/java/com/gestionSalleCefim/Group3/ReservationTests.java
+++ src/test/java/com/gestionSalleCefim/Group3/ReservationTests.java
@@ -1,37 +1,22 @@
 package com.gestionSalleCefim.Group3;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.gestionSalleCefim.Group3.entities.Reservation;
-import com.gestionSalleCefim.Group3.entities.User;
-import com.gestionSalleCefim.Group3.repositories.ReservationRepository;
+
 import com.gestionSalleCefim.Group3.services.ReservationService;
 import jakarta.transaction.Transactional;
-import jakarta.validation.constraints.AssertTrue;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.util.Assert;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Arrays;
+
+
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.Optional;
 
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
