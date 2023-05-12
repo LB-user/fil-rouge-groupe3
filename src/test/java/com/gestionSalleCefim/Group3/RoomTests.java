a9209f54655d51e67911a6d8f4ca4007864d2905
LB <brault.13.ludovic@gmail.com>
2023-05-12 17:19:34 +0200

    modifs RoomTests

--- src/test/java/com/gestionSalleCefim/Group3/RoomTests.java
+++ src/test/java/com/gestionSalleCefim/Group3/RoomTests.java
@@ -1,8 +1,8 @@
 package com.gestionSalleCefim.Group3;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gestionSalleCefim.Group3.entities.Building;
 import com.gestionSalleCefim.Group3.entities.Room;
-import com.gestionSalleCefim.Group3.entities.User;
 import com.gestionSalleCefim.Group3.repositories.RoomRepository;
 import com.gestionSalleCefim.Group3.services.RoomService;
 import jakarta.transaction.Transactional;
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,10 +26,10 @@ import java.util.Objects;
 @Transactional
 public class RoomTests {
     @Autowired
-    private RoomRepository RoomRepository;
+    private RoomRepository roomRepository;
 
     @Autowired
-    private RoomService RoomService;
+    private RoomService roomService;
 
     // Classe pour simuler des appels REST
     @Autowired
@@ -41,47 +40,130 @@ public class RoomTests {
     private ObjectMapper objectMapper;
 
     @Test
-    void testPrintAllRoomsName(){
-        List<Room> rooms = RoomRepository.findAll();
-        rooms.forEach(Room -> System.out.println(Room.getName()));
-    }
-
-    @Test
-    void testGetAllRoomsByAPI() throws Exception {
-        // Création de notre requête au moyen de la classe MockMvcRequestBuilders
-        // Utilisation de la méthode correspondant au verbe HTTP voulu, qui prend en paramètre l'URL du point d'API
-        RequestBuilder request = MockMvcRequestBuilders.get("/api/room/all");
-        // Test du status de la réponse, ici 200 (isOk())
-        ResultMatcher resultStatus = MockMvcResultMatchers.status().isOk();
-        String contentAsString = mockMvc.perform(request)
-                .andExpect(resultStatus)
-                .andReturn().getResponse().getContentAsString();
-
-        // Désérialisation du contenu de la réponse en List<Book>
-        List<Room> rooms = Arrays.asList(objectMapper.readValue(contentAsString, Room[].class));
-
-        Room roomWithId9 = RoomService.getById(9);
-        boolean roomWithId9AndSameNameExists = rooms.stream()
-                .filter(room -> room.getId() == 9)
-                .anyMatch(room -> room.getName().equals(roomWithId9.getName()));
-
-        Assertions.assertTrue(roomWithId9AndSameNameExists);
-    }
-
-    @Test
     void testPostRoom() throws Exception {
         Room room = new Room();
-        room.setName("RoomTest");
+        room.setName("Salle Test");
+        room.setCapacity(11);
+        room.setLocation("Lieu Test");
+        room.setEquipment("Equipements fictifs");
+        Building building = new Building();
+        building.setId(1);
+        room.setBuilding(building);
+
         RequestBuilder request = MockMvcRequestBuilders.post("/api/room")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(room));
 
-        ResultMatcher resultStatus = MockMvcResultMatchers.status().isOk();
+        ResultMatcher resultStatus = MockMvcResultMatchers.status().isCreated();
         String contentAsString = mockMvc.perform(request)
                 .andExpect(resultStatus)
                 .andReturn().getResponse().getContentAsString();
 
         Room newRoom = objectMapper.readValue(contentAsString, Room.class);
-        assert Objects.equals(newRoom.getName(), room.getName());
+        Assertions.assertNotNull(newRoom.getId());
+    }
+
+    @Test
+    void testPostRoomIdExists() throws Exception {
+
+        Room room = new Room();
+        room.setId(1);
+        room.setName("Salle Test");
+        room.setCapacity(11);
+        room.setLocation("Lieu Test");
+        room.setEquipment("Equipements fictifs");
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/api/room")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(room));
+
+        ResultMatcher resultStatus = MockMvcResultMatchers.status().isConflict();
+        String contentAsString = mockMvc.perform(request)
+                .andExpect(resultStatus)
+                .andReturn().getResponse().getContentAsString();
+
+        Room newRoom = objectMapper.readValue(contentAsString, Room.class);
+        assert !Objects.equals(newRoom.getName(), room.getName());
+    }
+
+    @Test
+    void testPrintAllRoomsName(){
+        List<Room> rooms = roomRepository.findAll();
+        rooms.forEach(room -> System.out.println(room.getName()));
+    }
+
+    @Test
+    void testFindRoomById() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/room/{id}", 1);
+
+        ResultMatcher resultStatus = MockMvcResultMatchers.status().isOk();
+        String contentAsString = mockMvc.perform(request)
+                .andExpect(resultStatus)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Salle 1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.capacity").value(25))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.location").value("Bâtiment A"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.equipment").value("Table et chaise"))
+                .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void testFindRoomByIdError() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/room/{id}", 1);
+
+        ResultMatcher resultStatus = MockMvcResultMatchers.status().isOk();
+        String contentAsString = mockMvc.perform(request)
+                .andExpect(resultStatus)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Salle 1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.capacity").value(24))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.location").value("Bâtiment A"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.equipment").value("Table et chaise"))
+                .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void testGetAllRoomsByAPI() {
+        MockMvcRequestBuilders.get("/api/room/all");
+
+        MockMvcResultMatchers.status().isOk();
+    }
+
+    @Test
+    public void testUpdateRoom() throws Exception
+    {
+        Room room = new Room();
+        room.setId(1);
+        room.setName("Salle Test");
+        room.setCapacity(11);
+        room.setLocation("Lieu Test");
+        room.setEquipment("Equipements fictifs");
+        RequestBuilder request = MockMvcRequestBuilders.put("/api/room")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(room));
+
+        ResultMatcher resultStatus = MockMvcResultMatchers.status().isOk();
+        mockMvc.perform(request)
+                .andExpect(resultStatus)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Salle Test"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.capacity").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.location").value("Lieu Test"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.equipment").value("Equipements fictifs"))
+                .andReturn().getResponse().getContentAsString();
+
+    }
+
+    @Test
+    void testDeleteRoom() throws Exception {
+        Room room = roomRepository.findById(1).orElse(null);
+        Assertions.assertNotNull(room);
+
+        mockMvc.perform( MockMvcRequestBuilders.delete("/api/room/{id}", 1) )
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        Room room1 = roomRepository.findById(1).orElse(null);
+        Assertions.assertNull(room1);
     }
 }
